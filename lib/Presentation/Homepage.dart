498c30b366b9dc30ef080ee1622749c8ca3404d4
Hiren Makwana <hirenm563@gmail.com>
2023-10-03 00:00:59 +0530

    #6 Modifying Answer

--- lib/Presentation/Homepage.dart
+++ lib/Presentation/Homepage.dart
@@ -15,21 +15,22 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<QuizModel>? _quizModel;
   bool quizdata = true;
+  int lastSequence = 0;
+  int totallength = 0;
 
   @override
   void initState() {
     QuizApi().quizap().then((value) {
       setState(() {
         _quizModel = value;
-
+        log(value.toString());
         log("===== Quiz APi Calling =====");
         quizdata = false;
+        totallength = _quizModel!.length;
       });
     });
     super.initState();
   }
-
-  //Every time data change thy che value.first ma 1st j ayo to
 
   @override
   Widget build(BuildContext context) {
@@ -54,72 +55,195 @@ class _HomePageState extends State<HomePage> {
                 "TRUE FALSE",
               )
             ]),
-            Flexible(
-              child: Swiper(
-                  itemCount: _quizModel!.length,
-                  allowImplicitScrolling: true,
-                  // index: 2,
-                  layout: SwiperLayout.DEFAULT,
-                  itemWidth: 300.0,
-                  itemHeight: 400.0,
-                  transformer: ScaleAndFadeTransformer(),
-                  viewportFraction: 0.8,
-                  scale: 0.9,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      child: Container(
-                        margin: const EdgeInsets.all(10),
-                        child: Column(
-                          children: [
-                            Container(
-                              // margin: EdgeInsets.all(10),
-                              child: const Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Text(
-                                        "Questions ",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      Text(
-                                        "5 / ",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      Text(
-                                        "10 ",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                    ],
-                                  ),
-                                  Row(
-                                    children: [Icon(Icons.more_vert)],
-                                  )
-                                ],
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 30,
-                            ),
-                            Row(
+            !quizdata
+                ? Flexible(
+                    child: Swiper(
+                        itemCount: _quizModel!.length,
+                        allowImplicitScrolling: true,
+                        // index: 2,
+                        layout: SwiperLayout.DEFAULT,
+                        itemWidth: 300.0,
+                        itemHeight: 400.0,
+                        transformer: ScaleAndFadeTransformer(),
+                        viewportFraction: 0.8,
+                        scale: 0.9,
+                        itemBuilder: (context, index) {
+                          int sequence = index + 1;
+                          lastSequence = sequence;
+
+                          return SingleChildScrollView(
+                            child: Column(
                               children: [
-                                Flexible(
-                                    child: Text(
-                                        _quizModel![index].question.toString()))
+                                Card(
+                                  child: Container(
+                                    margin: const EdgeInsets.all(10),
+                                    child: Column(
+                                      children: [
+                                        Container(
+                                          // margin: EdgeInsets.all(10),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Row(
+                                                children: [
+                                                  Text(
+                                                    "Questions ",
+                                                    style: TextStyle(
+                                                        fontWeight:
+                                                            FontWeight.bold),
+                                                  ),
+                                                  Text(
+                                                    "$lastSequence / ",
+                                                    style: TextStyle(
+                                                        fontWeight:
+                                                            FontWeight.bold),
+                                                  ),
+                                                  Text(
+                                                    totallength.toString(),
+                                                    style: TextStyle(
+                                                        fontWeight:
+                                                            FontWeight.bold),
+                                                  ),
+                                                ],
+                                              ),
+                                              Row(
+                                                children: [
+                                                  Icon(Icons.more_vert)
+                                                ],
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                          height: 30,
+                                        ),
+                                        Row(
+                                          children: [
+                                            Flexible(
+                                                child: Text(
+                                              _quizModel![index]
+                                                  .question
+                                                  .toString(),
+                                              style: TextStyle(
+                                                  color: Color.fromARGB(
+                                                      255, 142, 87, 215),
+                                                  fontWeight: FontWeight.bold),
+                                            ))
+                                          ],
+                                        ),
+                                        const SizedBox(
+                                          height: 30,
+                                        ),
+                                        GestureDetector(
+                                          onTap: () {},
+                                          child: Container(
+                                            width: 240,
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(30),
+                                                color: Colors.transparent,
+                                                border: Border.all(
+                                                    color: Color.fromARGB(
+                                                        255, 142, 87, 215))),
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(8),
+                                              child: Text("A." +
+                                                  _quizModel![index]
+                                                      .answers!
+                                                      .answerA
+                                                      .toString()),
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: 20,
+                                        ),
+                                        GestureDetector(
+                                          onTap: () {},
+                                          child: Container(
+                                            width: 240,
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(30),
+                                                color: Colors.transparent,
+                                                border: Border.all(
+                                                    color: Color.fromARGB(
+                                                        255, 142, 87, 215))),
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(8),
+                                              child: Text("B." +
+                                                  _quizModel![index]
+                                                      .answers!
+                                                      .answerB
+                                                      .toString()),
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: 20,
+                                        ),
+                                        GestureDetector(
+                                          onTap: () {},
+                                          child: Container(
+                                            width: 240,
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(30),
+                                                color: Colors.transparent,
+                                                border: Border.all(
+                                                    color: Color.fromARGB(
+                                                        255, 142, 87, 215))),
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(8),
+                                              child: Text("C." +
+                                                  _quizModel![index]
+                                                      .answers!
+                                                      .answerC
+                                                      .toString()),
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: 20,
+                                        ),
+                                        GestureDetector(
+                                          onTap: () {},
+                                          child: Container(
+                                            width: 240,
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(30),
+                                                color: Colors.transparent,
+                                                border: Border.all(
+                                                    color: Color.fromARGB(
+                                                        255, 142, 87, 215))),
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.all(8.0),
+                                              child: Text("D." +
+                                                  _quizModel![index]
+                                                      .answers!
+                                                      .answerD
+                                                      .toString()),
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: 20,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
                               ],
-                            )
-                          ],
-                        ),
-                      ),
-                    );
-                  }),
-            ),
+                            ),
+                          );
+                        }),
+                  )
+                : Center(child: CircularProgressIndicator()),
             Padding(
-              padding: const EdgeInsets.only(bottom: 20.0, top: 20),
+              padding: const EdgeInsets.only(bottom: 20.0, top: 10),
               child: Center(
                 child: SizedBox(
                   width: 270,
